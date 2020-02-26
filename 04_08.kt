b478af16ca446fa260937a527ebd2eb489929179
Chogrammer26 <48506056+Chogrammer26@users.noreply.github.com>
2020-02-26 21:18:04 +0800

    Create 04_08.kt

---
+++ 04_08.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+  var a = 1
+  var b = 1
+  var temp = a
+  var fibo:Int
+  var nthTerm: Int = 6
+  
+  if (nthTerm == 1 || nthTerm == 2)
+    fibo = 1
+  else {
+    while(nthTerm > 2) {
+      temp = a
+      a = b
+      b += temp
+      nthTerm--
+    }
+    
+    fibo = b
+    
+  }
+  
+  println(fibo)
+}
