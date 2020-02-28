091827b68e9e16f68cd3bdcfd65380aca22a976b
Chogrammer26 <48506056+Chogrammer26@users.noreply.github.com>
2020-02-28 10:29:36 +0800

    Update 06_01.kt

--- 06_01.kt
+++ 06_01.kt
@@ -1,1 +1,14 @@
+fun main(args: Array<String>) {
+    print(isPrime(4))
+}
 
+fun isPrime(num: Int): Boolean {
+    if (num < 0)
+    	return false
+    else {
+    	for (i in 2..num/2) 
+    		if (num % i == 0)
+        		return false
+    	return true
+    }
+}
