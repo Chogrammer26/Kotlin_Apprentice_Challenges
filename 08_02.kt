929d081df170e2055652a8cfbeea441eb26c626e
Chogrammer26 <48506056+Chogrammer26@users.noreply.github.com>
2020-03-03 11:03:58 +0800

    Update 08_02.kt

--- 08_02.kt
+++ 08_02.kt
@@ -1,12 +1,14 @@
 fun main() {
-    var numList = mutableListOf(5,6,5)
+    val numList: List<Int> = listOf(5,6,5)
     removeOne(5,numList) // [6,5]
     removeOne(5,numList) // [6]      
 }
 
-fun removeOne(num: Int, list: MutableList<Int>): Unit {
+fun removeOne(num: Int, list: List<Int>): List<Int> {
+    val numbers = list.toMutableList()
     if (num !in list)
     	println("$num not found")
     else
-    	list.remove(num)   
+    	list.remove(num)
+    return numbers
 }
