efbc56463dc4f05bfff91eb78fa090fff5a7fc96
Chogrammer26 <48506056+Chogrammer26@users.noreply.github.com>
2020-03-15 09:47:39 +0800

    Update 10_02.kt

--- 10_02.kt
+++ 10_02.kt
@@ -1,8 +1,17 @@
 fun main() {
     println(mathSum(10) {it * it})
-    //println(mathSum(10) {
-    //    
-    //})
+    println(mathSum(10) { i ->
+      var counter = 0
+      var a = 1
+      var b = 1 
+      var sum: Int
+      while (counter++ < i - 2) {
+          sum = a + b
+          a = b 
+          b = sum 
+      }
+      b
+    })
 }
 
 fun mathSum(length: Int, series: (Int) -> Int): Int {
